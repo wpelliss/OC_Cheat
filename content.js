8e3106f1b17ca033a55c2f9ced743b82cdb37105
wpelliss <wilfried.pellissard@hotmail.fr>
2019-03-04 15:56:09 +0100

    0.1.0 Update

--- content.js
+++ content.js
@@ -1,5 +1,9 @@
-if(window.location.hostname == "openclassrooms.com")
+if (window.location.hostname == "openclassrooms.com")
 {
-	var video = document.getElementsByTagName("video")[0];
-	video.parentNode.innerHTML = '<aside id="r-3850508" data-claire-element-id="6260283" data-claire-semantic="information" class=""><p id="r-3850507" data-claire-element-id="6260282">' + "<a href=\"" + video.src + "\"> Vidéo originale </a>" + '</p></aside>' + video.parentNode.innerHTML;
+	var videos = document.getElementsByTagName("video");
+	for (var i = 0; i < videos.length; i++)
+	{
+		var video = videos[i];
+		video.parentNode.innerHTML = '<aside id="r-3850508" data-claire-element-id="6260283" data-claire-semantic="information" class=""><p id="r-3850507" data-claire-element-id="6260282">' + "<a href=\"" + video.src + "\"> Vidéo originale </a>" + '</p></aside>' + video.parentNode.innerHTML;
+	}
 }
